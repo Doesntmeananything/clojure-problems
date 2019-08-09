84d8660ea3e8898c7f4794726d1ee02c114c8021
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-08-09 23:37:50 +0300

    Update challenges

--- src/clojure_problems/core.clj
+++ src/clojure_problems/core.clj
@@ -1,6 +1,10 @@
 (ns clojure-problems.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+"Write a function which returns the second to last element from a sequence."
+(= (fn [x] (nth x (- (count x) 2)) (list 1 2 3 4 5)) 4)
+
+"Write a function which returns the Nth element from a sequence."
+(= (#(last (take (inc %2) %1))'(4 5 6 7) 2) 6)
+
+"Write a function which returns the total number of elements in a sequence."
+(= (__ '(1 2 3 3 1)) 5)
