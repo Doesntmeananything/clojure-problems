0710971604fbceda25bf7175ec61e32023eec853
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-08-12 21:02:03 +0300

    Update with new problems

--- src/clojure_problems/core.clj
+++ src/clojure_problems/core.clj
@@ -8,8 +8,8 @@
 
 "Write a function which returns the total number of elements in a sequence."
 (= (#(reduce
-       (fn [c _] (inc c))
-       0 %) '(1 2 3 3 1)) 5)
+      (fn [c _] (inc c))
+      0 %) '(1 2 3 3 1)) 5)
 
 "Write a function which reverses a sequence."
 (= ((fn [seq] (into '() seq)) [1 2 3 4 5]) [5 4 3 2 1])
@@ -64,10 +64,10 @@
 
 "Write a function which replicates each element of a sequence a variable number of times."
 (= ((fn f [s n]
-       (let [f*
-             (fn [acc s*]
-               (concat acc (repeat n s*)))]
-         (reduce f* [] s))) [1 2 3] 2) '(1 1 2 2 3 3))
+      (let [f*
+            (fn [acc s*]
+              (concat acc (repeat n s*)))]
+        (reduce f* [] s))) [1 2 3] 2) '(1 1 2 2 3 3))
 
 "Write a function which creates a list of all integers in a given range."
 (= ((fn [s e]
@@ -75,3 +75,66 @@
         (if (< x e)
           (recur (inc x) (conj acc x))
           acc))) 1 4) '(1 2 3))
+
+"Write a function which creates a list of all integers in a given range."
+(= ((fn f [s e]
+      (loop [x s acc []]
+        (if (< x e)
+          (recur (inc x) (conj acc x))
+          acc))) 1 4) '(1 2 3))
+
+"Write a function which takes a variable number of parameters and returns the maximum value."
+(= ((fn [& s]
+      (reduce (fn [acc s*]
+                (if (> s* acc)
+                  s*
+                  acc))
+              (first s)
+              (rest s))) 1 8 3 4) 8)
+
+"Write a function which takes two sequences and returns the first item from each, then the second item from each, then the third, etc."
+(= (#(mapcat list %1 %2) [1 2 3] [:a :b :c]) '(1 :a 2 :b 3 :c))
+
+"Write a function which separates the items of a sequence by an arbitrary value."
+(= ((fn [x s]
+      (rest (mapcat #(list x %1) s))) 0 [1 2 3]) [1 0 2 0 3])
+
+"Write a function which drops every Nth item from a sequence."
+(= ((fn [s n]
+      (loop [agg [] s* s]
+        (if (empty? s*)
+          agg
+          (recur (concat agg (let [x (take n s*)]
+                               (if (> n (count x))
+                                 x
+                                 (drop-last x))))
+                 (drop n s*))))) [1 2 3 4 5 6 7 8] 3) [1 2 4 5 7 8])
+
+"Write a function which calculates factorials."
+(= ((fn [n]
+      (loop [cnt n acc 1]
+        (if (zero? cnt)
+          acc
+          (recur (dec cnt) (* acc cnt))))) 5) 120)
+
+"Write a function which reverses the interleave process into x number of subsequences."
+(= ((fn [s n]
+      (let [s* (partition n n s)]
+        (for [i (range n)]
+          (map #(nth %1 i) s*)))) [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
+
+"Write a function which can rotate a sequence in either direction."
+(= ((fn [n s]
+      (let [n* (rem n (count s))]
+        (if (pos? n*)
+          (concat (drop n* s) (take n* s))
+          (concat (take-last (unchecked-negate n*) s) (drop-last (unchecked-negate n*) s))))) 2 [1 2 3 4 5]) '(3 4 5 1 2))
+
+"Write a higher-order function which flips the order of the arguments of an input function."
+(= 3 (((fn [f]
+         (fn [& args]
+           (apply f (reverse args)))) nth) 2 [1 2 3 4 5]))
+
+"Write a function which will split a sequence into two parts."
+(= ((fn [n s]
+      (list (take n s) (drop n s))) 3 [1 2 3 4 5 6]) [[1 2 3] [4 5 6]])
